1b16e06a7f70e76e941714a3596f0110492c7521
Ritik <ritik.k.1404@gmail.com>
2024-09-15 07:19:12 +0530

    Initial Commit

---
+++ swiftUINotes/swiftUINotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftUINotesApp.swift
+//  swiftUINotes
+//
+//  Created by Ritik Karma on 15/09/24.
+//
+
+import SwiftUI
+
+@main
+struct swiftUINotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
