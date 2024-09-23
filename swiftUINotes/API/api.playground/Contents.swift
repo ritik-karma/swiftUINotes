f170635e67cd0ccceeb75e9669dc436e999712e3
Ritik <ritik.k.1404@gmail.com>
2024-09-23 20:17:15 +0530

    notes

--- swiftUINotes/API/api.playground/Contents.swift
+++ swiftUINotes/API/api.playground/Contents.swift
@@ -1,3 +1,5 @@
+import SwiftUI
+
 struct User: Codable {
     let username: String
     let occupation: String
@@ -11,9 +13,16 @@ if
     ///Step 2 : Get the contents of the file as data so we can use that
     let data = FileManager.default.contents(atPath: path) {
     
-    let decode = JSONDecoder()
-    let user = try decode.decode(User.self, from: data)
+    let decode = JSONDecoder() ///Converts binary to data
+    let user = try decode.decode([User].self, from: data)
+    dump(user)  //detail print
+
+    
+    let encode = JSONEncoder() //Data to Binary(JSON format)
+    let data = try encode.encode(user)
+    dump(data)
+    
+    dump(String(data: data, encoding: .utf8)) ///attempts to convert the binary data into a human-readable JSON string and prints it.
     
 }
-
 
