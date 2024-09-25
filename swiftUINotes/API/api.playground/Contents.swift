c77cc720109d319aa7a168bda2b9482c6997d584
Ritik <ritik.k.1404@gmail.com>
2024-09-25 12:21:33 +0530

    notes

--- swiftUINotes/API/api.playground/Contents.swift
+++ swiftUINotes/API/api.playground/Contents.swift
@@ -1,8 +1,30 @@
+//Encoding: Data to binary
+
 import SwiftUI
+
+struct Occupation: Codable {
+    let name: String
+    let isActive: Bool
+}
+
 
 struct User: Codable {
     let username: String
-    let occupation: String
+    let id: Int
+    let firstName: String
+    let lastName: String?    //Made it Optional so it can be nill also
+    let occupation: Occupation
+    let dateOfBirth: Date
+    
+    //Enum will be used for mapping JSon keys & struct var if JSON key's are diff from structs keys like in this case is id/userID.
+    enum CodingKeys: String, CodingKey {
+        case id = "userID"
+        case firstName
+        case lastName
+        case username
+        case occupation
+        case dateOfBirth
+    }
 }
 
 
@@ -14,6 +36,12 @@ if
     let data = FileManager.default.contents(atPath: path) {
     
     let decode = JSONDecoder() ///Converts binary to data
+    
+    //Decoding Strategies
+    decode.keyDecodingStrategy = .convertFromSnakeCase  
+    decode.dateDecodingStrategy = .iso8601 //decode dates in iso formate
+    
+    
     let user = try decode.decode([User].self, from: data)
     dump(user)  //detail print
 
